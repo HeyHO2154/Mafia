d2249661d834ba6210c3284dbb36658486b072ab
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2024-10-18 09:09:54 +0900

    프론트 분리

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,5 +1,8 @@
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
+import 'CounterTest.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,65 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: CounterPage(),
-    );
-  }
-}
-
-class CounterPage extends StatefulWidget {
-  @override
-  _CounterPageState createState() => _CounterPageState();
-}
-
-class _CounterPageState extends State<CounterPage> {
-  int _counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _getCounterValue();
-  }
-
-  Future<void> _incrementCounter() async {
-    final response = await http.post(Uri.parse('http://10.0.2.2:8080/api/increment'));
-    if (response.statusCode == 200) {
-      print("정상 작동");
-    } else {
-      print('숫자 증가 실패: ${response.statusCode}');
-    }
-  }
-
-  Future<void> _getCounterValue() async {
-    final response = await http.get(Uri.parse('http://10.0.2.2:8080/api/value'));
-    if (response.statusCode == 200) {
-      setState(() {
-        _counter = int.parse(response.body);
-      });
-      print("정상 작동");
-    } else {
-      print('숫자 불러오기 실패: ${response.statusCode}');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Counter App')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Counter Value: $_counter'),
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: Text('A 버튼 (숫자 증가)'),
-            ),
-            ElevatedButton(
-              onPressed: _getCounterValue,
-              child: Text('B 버튼 (숫자 조회)'),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
