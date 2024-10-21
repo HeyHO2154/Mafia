be8bd18561ce4295f5d1aaa95d59c2d57796d0b6
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2024-10-21 14:45:14 +0900

    문장 변경

--- Frontend/lib/Game/Vote.dart
+++ Frontend/lib/Game/Vote.dart
@@ -6,7 +6,7 @@ class Vote extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Text(
-          'Players are voting!',
+          '투표를 시작합니다',
           style: TextStyle(fontSize: 24),
         ),
       ),
